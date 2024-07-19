5cee77f87746e5fae447121eb858a1ba6e778896
Hen17Ry <106038140+Hen17Ry@users.noreply.github.com>
2024-07-19 19:42:06 +0100

    Commit

--- functions/send-email.js
+++ functions/send-email.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Configure CORS
 app.use(cors());
