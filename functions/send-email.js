6f50094d14e7fefce19ee82e48accb01e52a1297
Hen17Ry <106038140+Hen17Ry@users.noreply.github.com>
2024-07-23 15:45:16 +0100

    Commit

--- functions/send-email.js
+++ functions/send-email.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const handler = async (event, context) => {
   // Set CORS headers
   const headers = {
-    'Access-Control-Allow-Origin': '*', // Autoriser toutes les origines
+    'Access-Control-Allow-Origin': 'http://localhost:5173', // Autoriser toutes les origines
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Credentials': 'true'
